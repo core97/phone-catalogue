811ba0546e7cf52e9fdd4745de053b830224a720
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-02 17:57:35 +0200

    feat: create phone domain

---
+++ server/models/entities/phone.domain.ts
@@ -0,0 +1,37 @@
+import { Manufacter } from 'server/shared/types';
+import { Phone as PhoneEntity } from './phone.entity';
+
+export class Phone implements PhoneEntity {
+  id: string;
+
+  color: string;
+
+  createdAt: Date;
+
+  description: string;
+
+  imageFileName: string;
+
+  manufacturer: Manufacter;
+
+  name: string;
+
+  price: number;
+
+  processor: string;
+
+  ram: number;
+
+  constructor(phone: Phone) {
+    this.id = phone.id;
+    this.createdAt = new Date(phone.createdAt);
+    this.name = phone.name;
+    this.manufacturer = phone.manufacturer;
+    this.description = phone.description;
+    this.color = phone.color;
+    this.price = phone.price;
+    this.imageFileName = phone.imageFileName;
+    this.processor = phone.processor;
+    this.ram = phone.ram;
+  }
+}
