b1e74bf97b9edf1f4f1bb50fd16251a245d83e7c
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-02 18:06:42 +0200

    feat: create Schema

---
+++ server/shared/schema.ts
@@ -0,0 +1,30 @@
+import { ObjectSchema } from 'joi';
+
+export class Schema<T> {
+  readonly objectSchema: ObjectSchema<T>;
+
+  private readonly schemaName: string;
+
+  constructor(objectSchema: ObjectSchema<T>, schemaName: string) {
+    this.objectSchema = objectSchema;
+    this.schemaName = schemaName;
+  }
+
+  getValidatedValue(data: Partial<T>): T {
+    const { error, value, warning } = this.objectSchema.validate(data);
+
+    if (error || warning) {
+      const msg = error?.message || warning?.message;
+
+      throw new Error(
+        `[${this.schemaName}]: an error ocurred when validate value.\n${msg}`
+      );
+    }
+
+    return value;
+  }
+
+  getSchema(): ObjectSchema<T> {
+    return this.objectSchema;
+  }
+}
