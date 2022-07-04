d605f76c232031f887dbc0ef32ca924daf6330ca
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-05 00:25:13 +0200

    chore: add toast configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
 import { Layout } from 'components/Layout';
+import 'react-toastify/dist/ReactToastify.css';
 import 'styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Layout>
     <Component {...pageProps} />
+    <ToastContainer position='bottom-center' />
   </Layout>
 );
 
