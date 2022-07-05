a8b5d1270a900cf7582d310aee68c379adfab0e5
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-05 09:14:00 +0200

    chore: add react-query configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,36 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider, Hydrate } from 'react-query';
 import { ToastContainer } from 'react-toastify';
 import { Layout } from 'components/Layout';
 import 'react-toastify/dist/ReactToastify.css';
 import 'styles/globals.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Layout>
-    <Component {...pageProps} />
-    <ToastContainer position='bottom-center' />
-  </Layout>
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+            staleTime: 60 * 1000, // 1 minute
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate>
+        <Layout>
+          <Component {...pageProps} />
+          <ToastContainer position="bottom-center" />
+        </Layout>
+      </Hydrate>
+    </QueryClientProvider>
+  );
+};
 
 export default MyApp;
