1ec568b4d835eb7dd284d480b334795c1240fcdb
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-05 23:37:06 +0200

    feat: create endpoint to delete phone

--- pages/api/phones/[id].ts
+++ pages/api/phones/[id].ts
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nextConnect from 'next-connect';
-import { getPhonesCtrl } from 'server/controllers/phone/get-phone.controller';
+import { getPhoneCtrl } from 'server/controllers/phone/get-phone.controller';
+import { deletePhoneCtrl } from 'server/controllers/phone/delete-phone.controller';
 
 const handler = nextConnect<NextApiRequest, NextApiResponse>();
 
-handler.get(async (req, res) => getPhonesCtrl.execute(req, res));
+handler.get(async (req, res) => getPhoneCtrl.execute(req, res));
+
+handler.delete(async (req, res) => deletePhoneCtrl.execute(req, res));
 
 export default handler;
