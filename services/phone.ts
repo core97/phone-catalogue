191e2a21b6e4b9a039b720d681e3f86fead8df5c
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-08 20:42:01 +0200

    refactor: create and use endpoints enum

--- services/phone.ts
+++ services/phone.ts
@@ -1,25 +1,24 @@
 import { httpInstance } from 'services/http-instance';
 import { Phone } from 'types/models';
+import { Endpoints } from 'types/endpoints';
 
-export const savePhone = async (phone: Phone): Promise<void> => {
-  const endpoint = '/phones';
-  await httpInstance.put(endpoint, phone);
+export const savePhone = async (phone: Phone) => {
+  await httpInstance.put(Endpoints.SAVE_PHONE, phone);
 };
 
-export const getPhones = async (): Promise<Phone[]> => {
-  const endpoint = '/phones';
-  const res = await httpInstance.get(endpoint);
+export const getPhones = async () => {
+  const res = await httpInstance.get<Phone[]>(Endpoints.GET_PHONES);
   return res.data;
 };
 
-export const getPhone = async (phoneId: string): Promise<Phone> => {
-  const endpoint = `/phones/${phoneId}`;
-  const res = await httpInstance.get(endpoint);
+export const getPhone = async (phoneId: string) => {
+  const endpoint = `${Endpoints.GET_PHONE}${phoneId}`;
+  const res = await httpInstance.get<Phone>(endpoint);
   return res.data;
 }
 
-export const deletePhone = async (phoneId: string): Promise<void> => {
-  const endpoint = `/phones/${phoneId}`;
+export const deletePhone = async (phoneId: string) => {
+  const endpoint = `${Endpoints.DELETE_PHONE}${phoneId}`;
   const res = await httpInstance.delete(endpoint);
   return res.data;
 }
---
+++ types/endpoints.ts
@@ -0,0 +1,6 @@
+export enum Endpoints {
+  SAVE_PHONE = '/phones',
+  GET_PHONES = '/phones',
+  GET_PHONE = '/phones/',
+  DELETE_PHONE = '/phones/',
+}
