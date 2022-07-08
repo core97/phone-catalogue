b58fca5ab9142ee6b9e594b9e9af1ba9ab7293a8
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-08 14:23:19 +0200

    feat: add more texts in translations

--- locales/en.ts
+++ locales/en.ts
@@ -8,6 +8,50 @@ const EN_TRANSLATIONS: TranslatedTexts = {
     },
     footer: 'Code challenge by Juan Cortés ✌️',
   },
+  globalMsg: {
+    loading: 'Loading...',
+    error: 'Sorry an error occurred.',
+    notFoundResults: 'Not found results',
+    features: 'Features'
+  },
+  pages: {
+    home: {
+      header: 'Phone list'
+    },
+    phoneForm: {
+      creationHeader: '📱☎️ Phone',
+    }
+  },
+  phone: {
+    fields: {
+      name: 'Name',
+      description: 'Description',
+      price: 'Price',
+      color: 'Color',
+      manufacturer: 'Manufacturer',
+      processor: 'Processor',
+      ram: 'RAM',
+    },
+    messages: {
+      deletionLoading: 'Wait, we are deleting the phone',
+      deletionSuccess: 'The phone has been deleted successfully',
+      deleteBtn: 'Delete phone',
+    },
+  },
+  forms: {
+    phone: {
+      loadingCreation: 'Wait, we are creating the phone.',
+      successCreation: 'Congratulations, the phone has been successfully created.',
+      placeholders: {
+        name: 'Samsung Galaxy S9',
+        description: 'Lorem ipsum',
+        price: '499',
+        processor: 'Qualcon',
+        ram: '8',
+      },
+      submitCreationBtn: 'Create phone',
+    },
+  },
 };
 
 export default EN_TRANSLATIONS;
--- locales/es.ts
+++ locales/es.ts
@@ -5,7 +5,51 @@ const ES_TRANSLATIONS = {
       logo: 'Catálogo',
     },
     footer: 'Code challenge by Juan Cortés ✌️',
-  }
+  },
+  globalMsg: {
+    loading: 'Cargando...',
+    error: 'Lo sentimos ha ocurrido un error.',
+    notFoundResults: 'No se han encontrado resultados',
+    features: 'Características'
+  },
+  pages: {
+    home: {
+      header: 'Lista de móviles'
+    },
+    phoneForm: {
+      creationHeader: '📱☎️ Móvil',
+    }
+  },
+  phone: {
+    fields: {
+      name: 'Nombre',
+      description: 'Descripción',
+      price: 'Precio',
+      color: 'Color',
+      manufacturer: 'Fabricante',
+      processor: 'Procesador',
+      ram: 'RAM',
+    },
+    messages: {
+      deletionLoading: 'Espere, estamos borrando el teléfono',
+      deletionSuccess: 'El teléfono se ha borrado correctamente',
+      deleteBtn: 'Borrar teléfono',
+    },
+  },
+  forms: {
+    phone: {
+      loadingCreation: 'Espere, estamos creando el teléfono.',
+      successCreation: 'Enhorabuena, el teléfono se ha creado correctamente.',
+      placeholders: {
+        name: 'Samsung Galaxy S9',
+        description: 'Lorem ipsum',
+        price: '499',
+        processor: 'Qualcon',
+        ram: '8',
+      },
+      submitCreationBtn: 'Crear móvil',
+    },
+  },
 };
 
 export default ES_TRANSLATIONS;
