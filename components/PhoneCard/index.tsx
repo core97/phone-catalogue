78abc165ceee6d8b4c5db30091a4f7907e955ff7
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-08 10:06:31 +0200

    feat: change components styles

--- components/PhoneCard/index.tsx
+++ components/PhoneCard/index.tsx
@@ -6,7 +6,7 @@ import styles from './PhoneCard.module.css';
 
 export const PhoneCard = ({ id, image, title }: Props) => (
   <Link href={`/phone/${id}`}>
-    <a className={styles['card-wrapper']}>
+    <a>
       <figure className={styles['image-wrapper']}>
         <Image src={image} width={500} height={500} layout="responsive" />
       </figure>
