c7bd298f958f56f5a1ea30682f7c42eab3e65d77
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-08 10:00:14 +0200

    feat: create translations texts

---
+++ locales/en.ts
@@ -0,0 +1,13 @@
+import { TranslatedTexts } from 'types/language';
+
+const EN_TRANSLATIONS: TranslatedTexts = {
+  layout: {
+    navbar: {
+      createPhoneBtn: 'Create phone',
+      logo: 'Catalogue',
+    },
+    footer: 'Code challenge by Juan Cortés ✌️',
+  },
+};
+
+export default EN_TRANSLATIONS;
---
+++ locales/es.ts
@@ -0,0 +1,11 @@
+const ES_TRANSLATIONS = {
+  layout: {
+    navbar: {
+      createPhoneBtn: 'Crear teléfono',
+      logo: 'Catálogo',
+    },
+    footer: 'Code challenge by Juan Cortés ✌️',
+  }
+};
+
+export default ES_TRANSLATIONS;
