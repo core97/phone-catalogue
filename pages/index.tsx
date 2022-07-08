99b5d6a7f38964da638e3419d26c942ace253b89
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-08 10:13:56 +0200

    feat: create buttons to change language

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,25 @@ import type { NextPage } from 'next';
 import { Container } from 'components/Container';
 import { PhoneList } from 'components/PhoneList';
 import { Text } from 'components/Text';
+import { Button } from 'components/Button';
 import { AsyncComponent } from 'components/AsyncComponent';
 import { usePhonesQuery } from 'hooks/queries';
+import { useTranslation } from 'hooks/useTranslation';
+import { Language } from 'types/language';
 
 const HomePage: NextPage = () => {
   const phonesQuery = usePhonesQuery();
+  const { onChangeTranslation } = useTranslation();
 
   return (
     <Container size="xl">
+      <Container display="flex" justifyContent="space-around">
+        {[Language.ES, Language.EN].map(lang => (
+          <Button key={lang} as="button" onClick={() => onChangeTranslation(lang)}>
+            {lang}
+          </Button>
+        ))}
+      </Container>
       <AsyncComponent
         isLoading={phonesQuery.isLoading}
         error={phonesQuery.error}
