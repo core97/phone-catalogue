61b88ae47523080cad48ad41bc10085c51a4edae
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-08 14:21:11 +0200

    refactor: remove comments

--- components/Button/Button.interface.ts
+++ components/Button/Button.interface.ts
@@ -1,18 +1,3 @@
-/* type ClickableProps = {
-  as: 'button';
-  disabled?: boolean;
-  isLoading?: boolean;
-  onClick?: (...args: any[]) => void;
-  type?: 'submit' | 'reset' | 'button';
-};
-
-type LinkProps = { as: 'link'; link: string };
-
-type Props = {
-  children: string;
-  isFullWidth?: boolean;
-} & (ClickableProps | LinkProps); */
-
 interface ButtonBaseProps {
   children: string;
   isFullWidth?: boolean;
