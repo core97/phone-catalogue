45c4542048c09471887c183d996237e1e257e120
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-10 00:01:19 +0200

    chore: remove unnecessary environment variable

--- services/http-instance.ts
+++ services/http-instance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const httpInstance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_DOMAIN}${process.env.NEXT_PUBLIC_BASE_URL_API}`,
+  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL_API}`,
   timeout: 10 * 60000,
 });
