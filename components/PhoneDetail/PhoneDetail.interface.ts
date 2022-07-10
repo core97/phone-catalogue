98be853f6c4de92d2962b29863307e7c8b232fc0
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-11 00:24:38 +0200

    refactor: replace Omit by Pick

--- components/PhoneDetail/PhoneDetail.interface.ts
+++ components/PhoneDetail/PhoneDetail.interface.ts
@@ -1,3 +1,15 @@
 import { Phone } from 'types/models';
 
-export interface PhoneDetailProps extends Omit<Phone, 'createdAt'> {}
+export interface PhoneDetailProps
+  extends Pick<
+    Phone,
+    | 'id'
+    | 'color'
+    | 'description'
+    | 'imageFileName'
+    | 'manufacturer'
+    | 'name'
+    | 'price'
+    | 'processor'
+    | 'ram'
+  > {}
