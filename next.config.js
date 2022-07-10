aa2b9f53dfdbbcbc6b8be07ccf612d7b4c6ccdf7
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-10 12:45:56 +0200

    chore: add domains of images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['phone-catalogue.vercel.app', 'localhost'],
+  },
   reactStrictMode: true,
   i18n: {
     locales: ['es', 'en'],
