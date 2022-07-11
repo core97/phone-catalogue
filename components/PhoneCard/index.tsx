7454c3faf1e5ba16b9d5a14356d0fafd105fa2b7
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-11 18:31:16 +0200

    feat: add hover styles on cards

--- components/PhoneCard/index.tsx
+++ components/PhoneCard/index.tsx
@@ -6,17 +6,14 @@ import { PhoneCardProps } from './PhoneCard.interface';
 import styles from './PhoneCard.module.css';
 
 export const PhoneCard = ({ id, image, title }: PhoneCardProps) => (
-  <Link href={`${Routes.PHONE_DETAIL}${id}`}>
-    <a>
-      <figure className={styles['image-wrapper']}>
-        <Image
-          src={image}
-          width={500}
-          height={500}
-          layout="responsive"
-        />
-      </figure>
-      <Text as="h4">{title}</Text>
-    </a>
-  </Link>
+  <section className={styles.card}>
+    <Link href={`${Routes.PHONE_DETAIL}${id}`}>
+      <a >
+        <figure className={styles['image-wrapper']}>
+          <Image src={image} width={500} height={500} layout="responsive" />
+        </figure>
+        <Text as="h4">{title}</Text>
+      </a>
+    </Link>
+  </section>
 );
