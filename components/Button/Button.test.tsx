4aff848dd91622323a405e7c733e42d1bcf88972
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-11 16:41:10 +0200

    test: add unique test of <Button>

--- components/Button/Button.test.tsx
+++ components/Button/Button.test.tsx
@@ -1,34 +1,41 @@
 import React from 'react';
-import { render, fireEvent, RenderResult } from '@testing-library/react';
+import {
+  render,
+  renderHook,
+  RenderHookResult,
+  RenderResult,
+} from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { useTranslation } from '../../hooks/useTranslation';
 import { Button } from './index';
 
 jest.mock('next/router', () => ({
   useRouter: () => ({ locale: 'es' }),
 }));
 
-describe('<Button />', () => {
+describe('<Button isLoading />', () => {
   const spyMock = jest.fn();
   const label = 'button_label';
   let component: RenderResult;
+  let translationHook: RenderHookResult<
+    ReturnType<typeof useTranslation>,
+    void
+  >;
 
   beforeEach(() => {
+    translationHook = renderHook(() => useTranslation());
     component = render(
-      <Button onClick={spyMock}>
+      <Button onClick={spyMock} isLoading>
         {label}
       </Button>
     );
   });
 
-  test('renders content', () => {
-    expect(component.container).toHaveTextContent(label);
-  });
+  test('display loading message when is loading', () => {
+    const button = component.getByText(
+      translationHook.result.current.translation.globalMsg.loading
+    );
 
-  test('clicking the button calls event handler once', () => {
-    const button = component.getByText(label);
-
-    fireEvent.click(button);
-
-    expect(spyMock).toHaveBeenCalledTimes(1);
+    expect(button).not.toHaveTextContent(label);
   });
 });
