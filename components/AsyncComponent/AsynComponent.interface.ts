34467dc9c4d2327b8c9dea12b1ae150b88c7f34d
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-13 13:40:15 +0200

    refactor: remove unnused <AsyncComponent> componet

--- components/AsyncComponent/AsynComponent.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface AsyncComponentProps<T> {
-  data: T;
-  error: unknown;
-  isLoading: boolean;
-  render: (successData: NonNullable<T>) => React.ReactNode | React.ReactNode[];
-  errorMessage?: string;
-  loadingMessage?: string;
-  noDataMessage?: string;
-}
