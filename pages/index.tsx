e2845c4dea74e913c0b3c2019a86659e19d1e436
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-13 13:43:19 +0200

    feat: add title in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import type { NextPage } from 'next';
 import { Container } from 'components/Container';
 import { PhoneList } from 'components/PhoneList';
+import { Text } from 'components/Text';
+import { useTranslation } from 'hooks/useTranslation';
 
-const HomePage: NextPage = () => (
-  <Container size="xl">
-    <PhoneList />
-  </Container>
-);
+const HomePage: NextPage = () => {
+  const { translation } = useTranslation();
+
+  return (
+    <Container size="xl">
+      <Text size="2xl">{translation.pages.home.header}</Text>
+      <PhoneList />
+    </Container>
+  );
+};
 
 export default HomePage;
