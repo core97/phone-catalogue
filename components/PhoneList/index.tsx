9f37a2330bfb6ad46f12bcde29ad82b6a19d0951
Juan Cortes <j.cortes@crowdfarming.com>
2022-07-13 13:45:11 +0200

    feat: add loading message in <PhoneList>

--- components/PhoneList/index.tsx
+++ components/PhoneList/index.tsx
@@ -1,10 +1,17 @@
 import { memo } from 'react';
 import { PhoneCard } from 'components/PhoneCard';
+import { Text } from 'components/Text';
 import { usePhonesQuery } from 'hooks/queries';
+import { useTranslation } from 'hooks/useTranslation';
 import styles from './PhoneList.module.css';
 
 export const PhoneList = memo(() => {
   const phonesQuery = usePhonesQuery();
+  const { translation } = useTranslation();
+
+  if (phonesQuery.isLoading) {
+    return <Text>{translation.globalMsg.loading}</Text>;
+  }
 
   return (
     <ul className={styles.list}>
